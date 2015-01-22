4e91312b3bfdcfe52b46ec43645ed5a720c6a4d1
JCMinaya <jcmina14@gmail.com>
2015-01-21 23:18:30 -0400

    Json modified

--- js/post.js
+++ js/post.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
+	$(this).scrollTop(0);
 	var category;
 	$('.pure-u-1-3').click( function() {
 		
 		//Getting the category clicked
 		category = $(this).html();
 		$("#label").html(category);
-		var url = 'https://raw.githubusercontent.com/JCMinaya/NearBy/gh-pages/categories.json';
+		var url = 'https://raw.githubusercontent.com/JCMinaya/NearBy/gh-pages/categories(2).json';
 	    $.ajax({
 	       type: 'GET',
 	        url: url,
